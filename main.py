10e9e000ddc21d62b3fe5948beca5492d8a00c80
archanakumari620120-eng <archanakumari620120@gmail.com>
2025-09-24 15:06:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def generate_concept_and_metadata():
 def generate_image_huggingface(prompt, model_id="stabilityai/stable-diffusion-xl-base-1.0"):
     """Generates an image using Hugging Face Inference API."""
     
-    API_URL =f"[https://api-inference.huggingface.co/models/](https://api-inference.huggingface.co/models/){model_id}"
+    API_URL =f"https://api-inference.huggingface.co/models/{model_id}"
     headers = {"Authorization": f"Bearer {HF_API_TOKEN}"}
     payload = {"inputs": f"Vertical (1080x1920), {prompt}, cinematic, high detail, trending on artstation"}
 
